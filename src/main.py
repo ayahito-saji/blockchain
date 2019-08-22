5273a0608db2adbb9fa432a49a5ee2be1413e90c
Ayahito Saji <ayahito_saji@outlook.jp>
2019-08-22 21:47:32 +0900

    Add: feat register new transition

--- src/main.py
+++ src/main.py
@@ -2,15 +2,28 @@
 
 class Blockchain:
     def __init__(self):
-        pass
+        self.chain = []
+        self.current_transactions = []
 
     def new_block(selfs):
         # create new block and push chain
         pass
 
-    def new_transaction(self):
-        # create new transaction and push list
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        """
+        create new transaction and push list
+        :param sender: <str> the address of sender
+        :param recipient: <str> the address of recipient
+        :param amount: <int> amount of sent items
+        :return: <int> the index of block including this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
