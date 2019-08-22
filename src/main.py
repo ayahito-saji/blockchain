3f1dfb7de4dc1562b7392b688d3d2ee7b5de6dca
Ayahito Saji <ayahito_saji@outlook.jp>
2019-08-22 22:12:31 +0900

    Add: feat register new block, hash, lask_block

--- src/main.py
+++ src/main.py
@@ -1,13 +1,32 @@
 # coding: UTF-8
 
+import hashlib
+import json
+from time import time
+
 class Blockchain:
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(selfs):
-        # create new block and push chain
-        pass
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+        create new block and push chain
+        :param proof: <int> the proof which is get from proof of work algorithm
+        :param previous_hash: <str> the hash of previous block
+        :return: <dict> a block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -27,10 +46,24 @@ class Blockchain:
 
     @staticmethod
     def hash(block):
-        # hash block
-        pass
+        """
+        hash block
+        :param block: <dict> block
+        :return: <str> hash
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
-        # return the last block of chain
-        pass
+        """
+        return the last block of chain
+        :return: the last block of chain
+        """
+        return self.chain[-1]
+
+if __name__ == "__main__":
+    blockchain = Blockchain()
+    print(blockchain.new_transaction("Taro", "Jiro", 3))
+    print(blockchain.current_transactions)
+    print(blockchain.new_block(proof=1))
