00a999e2e1f858543bb103da0b11dc11b8f4ed31
Ayahito Saji <ayahito_saji@outlook.jp>
2019-08-22 20:59:11 +0900

    Add: class Blockchain

---
+++ src/main.py
@@ -0,0 +1,23 @@
+# coding: UTF-8
+
+class Blockchain:
+    def __init__(self):
+        pass
+
+    def new_block(selfs):
+        # create new block and push chain
+        pass
+
+    def new_transaction(self):
+        # create new transaction and push list
+        pass
+
+    @staticmethod
+    def hash(block):
+        # hash block
+        pass
+
+    @property
+    def last_block(self):
+        # return the last block of chain
+        pass
